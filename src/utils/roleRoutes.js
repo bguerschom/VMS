2dc557d5f917c0f10d2a270928cee770b937e86f
bguerschom <148221717+bguerschom@users.noreply.github.com>
2025-02-19 14:49:29 +0200

    Update roleRoutes.js

--- src/utils/roleRoutes.js
+++ src/utils/roleRoutes.js
@@ -2,7 +2,7 @@ export const getRoleBasedDashboard = (role) => {
   switch (role?.toLowerCase()) {
     case 'admin':
       return '/admindashboard';
-    case 'security':
+    case 'security_guard':
       return '/securityguarddashboard';
     case 'supervisor':
       return '/supervisordashboard';
